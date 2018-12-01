aa7dc3f6f01cf202a3f558b723b52b59f54cf842
vbournac <valentin.bournac@gmail.com>
2018-12-01 16:57:44 +0100

    code updated

--- move_printer/script/print_tube_moveit.py
+++ move_printer/script/print_tube_moveit.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
- 
+
 import sys
 import copy
 import rospy
 import moveit_commander
 import moveit_msgs.msg
 import geometry_msgs.msg
-
+import math
 
 class print_tube_moveit:
   waypoints= []
@@ -14,64 +14,68 @@ class print_tube_moveit:
     print "============ Starting tutorial setup"
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('print_tube_moveit', anonymous=True)
- 
-    
-    print "/////////////////debug1"
+
+
     self.group = moveit_commander.MoveGroupCommander("printer_group")
-    print "/////////////////debug2"
     self.group.set_pose_reference_frame("base_link")
-    
+
     self.group.set_named_target('init_pose')
     self.group.go(wait=True)
-    
+
     self.pose_A = geometry_msgs.msg.Pose()
     self.pose_A .position.x = 0.05
     self.pose_A.position.y = 0.0
-    self.pose_A.position.z = 0.05
-    
+    self.pose_A.position.z = -0.2
+
     self.pose_B = geometry_msgs.msg.Pose()
     self.pose_B .position.x = 0.0
     self.pose_B.position.y = 0.05
-    self.pose_B.position.z = 0.05
-    
+    self.pose_B.position.z = -0.2
+
     self.pose_C = geometry_msgs.msg.Pose()
     self.pose_C .position.x = -0.05
     self.pose_C.position.y = 0.0
-    self.pose_C.position.z = 0.05
-   
+    self.pose_C.position.z = -0.2
+
     self.pose_D = geometry_msgs.msg.Pose()
     self.pose_D .position.x = 0.0
     self.pose_D.position.y = -0.05
-    self.pose_D.position.z = 0.05
-   
+    self.pose_D.position.z = -0.2
+
     rospy.sleep(3)
-  
+
   def move_to_position(self):
-	#self.group.set_joint_value_target(self.pose_B,True)
+
+	# self.waypoints.append(self.pose_A)
+	# self.waypoints.append(self.pose_B)
+	# self.waypoints.append(self.pose_C)
+	# self.waypoints.append(self.pose_D)
+	num_point =10
+	diameter = 0.1
+	height = 0.15
 	
-	self.current_pose=self.group.get_current_pose().pose
-	#self.current_pose.position.z=-0.025
-	print self.current_pose
-	#self.waypoints.append(self.group.get_current_pose().pose)
-	#self.waypoints.append(self.pose_A)
-	#self.waypoints.append(self.pose_B)
-	#self.waypoints.append(self.pose_C)
-	#self.waypoints.append(self.pose_D)
-	#print self.waypoints
+	self.current_pose= self.group.get_current_pose().pose
+	self.current_pose.position.z = -0.2
+	self.waypoints.append(self.group.get_current_pose().pose)
 	
-	#self.group.plan()
-	#self.group.go(True)
-	
-	#rospy.sleep(3)
-	#print 'do second move'
-	#(self.plan,self.fraction)=self.group.compute_cartesian_path(self.waypoints,0.01,1000)
-	#rospy.sleep(3)
-	#self.group.execute(self.plan)
+	for i in range(1, num_point):
+        	for j in range(1, num_point):
+			self.next_pose = geometry_msgs.msg.Pose()
+			self.next_pose.position.x = diameter/2*math.cos(2*3.14*j/num_point)
+			self.next_pose.position.y = diameter/2*math.sin(2*3.14*j/num_point)
+			self.next_pose.position.z = -0.2+i*height/num_point
+			self.waypoints.append(self.next_pose)
+
+
+
+	(self.plan,self.fraction)=self.group.compute_cartesian_path(self.waypoints,0.01,1000)
+	rospy.sleep(3)
+	self.group.execute(self.plan)
 	#print self.waypoints
 
 	rospy.sleep(3)
 
-  
+
 if __name__ == '__main__':
     try:
 	moveit=print_tube_moveit()
